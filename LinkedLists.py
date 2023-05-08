5c178f724f29ef0cf797622d2170cdf577a1df95
rcarnold <rcarnold@bbw.com>
2023-05-08 16:10:26 -0400

    Initial commit

--- LinkedLists.py
+++ LinkedLists.py
@@ -231,18 +231,5 @@ class LinkedList:
                 self.length-=1     
             
             current = current.next
-        
-        
-        
-my_linked_list = LinkedList(1)
-my_linked_list.append(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(3)
-my_linked_list.append(2)
-my_linked_list.append(4)
-my_linked_list.remove_duplicates()
-
-my_linked_list.print_list()
 
                                                                                                                     
